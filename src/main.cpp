32c09b52a4d09c36f5dd73b4f65b1fefacc056d5
SaThaphone <alickvannala@gmail.com>
2025-10-29 10:27:46 +0700

    Lab

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int button = 2;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int buttonstate = digitalRead(2);
+  
+  
+  if(buttonstate == 1){
+  	Serial.println("ON");
+    digitalWrite(LED_BUILTIN, 1);
+  }else{
+    Serial.println("OFF");
+    digitalWrite(LED_BUILTIN, 0);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
